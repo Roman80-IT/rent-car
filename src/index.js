7590f78c4fea9e943d97b3546e6a8f21a7c4e34f
Roman <jrj80mail@gmail.com>
2024-02-18 18:24:45 +0200

    fix Project-name

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/rent-car">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
