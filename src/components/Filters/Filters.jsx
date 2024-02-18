ef93ea9ee925b492a237ab296e39467e96abcfb3
Roman <jrj80mail@gmail.com>
2024-02-18 21:37:19 +0200

    add Filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,3 +1,108 @@
+import { useSelector } from 'react-redux';
+import {
+  Btn,
+  CustomArrow,
+  Form,
+  FormItemWrapper,
+  FromTo,
+  FromToContainer,
+  Label,
+  MilageWrapper,
+  Select,
+} from './Filters.styled';
+import { selectMaxPrice, selectMinPrice } from '../../redux/selectors';
+
+const makes = [
+  'Buick',
+  'Volvo',
+  'HUMMER',
+  'Subaru',
+  'Mitsubishi',
+  'Nissan',
+  'Lincoln',
+  'GMC',
+  'Hyundai',
+  'MINI',
+  'Bentley',
+  'Mercedes-Benz',
+  'Aston Martin',
+  'Pontiac',
+  'Lamborghini',
+  'Audi',
+  'BMW',
+  'Chevrolet',
+  'Mercedes-Benz',
+  'Chrysler',
+  'Kia',
+  'Land',
+];
+
 export const Filters = () => {
-  return <div>Filters</div>;
+  const maxPrice = useSelector(selectMaxPrice);
+  const minPrice = useSelector(selectMinPrice);
+
+  const createRange = (min, max, step) => {
+    const result = [];
+    for (let i = Math.ceil(min / 10) * 10; i <= max; i += step) {
+      result.push(i);
+    }
+    return result;
+  };
+
+  return (
+    <Form>
+      <label>
+        <Label>Car brand</Label>
+        <FormItemWrapper>
+          <Select name="brand" id="brand" width="224px">
+            <option value="" disabled selected hidden>
+              Enter the text
+            </option>
+            {makes.map(item => (
+              <option value={item}>{item}</option>
+            ))}
+          </Select>
+          <CustomArrow />
+        </FormItemWrapper>
+      </label>
+      <label>
+        <Label>Price/ 1 hour</Label>
+        <FormItemWrapper>
+          <Select name="price" id="price" width="125px">
+            <option value="" disabled selected hidden>
+              To $
+            </option>
+            {createRange(minPrice, maxPrice, 10).map(price => (
+              <option value={price}>{price}</option>
+            ))}
+          </Select>
+          <CustomArrow />
+        </FormItemWrapper>
+      </label>
+      <label>
+        <Label>Сar mileage / km</Label>
+        <FromToContainer>
+          <FormItemWrapper>
+            <FromTo>From</FromTo>
+            <MilageWrapper
+              type="number"
+              name="min-milege"
+              id="min-milege"
+              side="left"
+            />
+          </FormItemWrapper>
+          <FormItemWrapper>
+            <FromTo>To</FromTo>
+            <MilageWrapper
+              type="number"
+              name="max-milege"
+              id="max-milege"
+              side="right"
+            />
+          </FormItemWrapper>
+        </FromToContainer>
+      </label>
+      <Btn>Search</Btn>
+    </Form>
+  );
 };
